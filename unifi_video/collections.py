0c6c96526f64d943f3bec0321d38f97bac7f2731
yuppity <yuppity@wubbalubba.com>
2018-12-22 10:41:41 +0200

    Create UnifiVideoCollection to hold singles (cameras, recs, etc)

---
+++ unifi_video/collections.py
@@ -0,0 +1,18 @@
+try:
+    from six import itervalues
+except (ImportError, ModuleNotFoundError):
+    from ._six import itervalues
+
+class UnifiVideoCollection(dict):
+    def __init__(self, collection_type, *args, **kwargs):
+        self._collection_type = collection_type
+        self.update(*args, **kwargs)
+
+    def __iter__(self, *args, **kwargs):
+        return itervalues(self, **kwargs)
+
+    def add(self, single_dict):
+        if not isinstance(single_dict, self._collection_type):
+            raise ValueError('Cannot add items other than of type {}'.format(
+                self._collection_type.__name__))
+        self[single_dict._id] = single_dict
