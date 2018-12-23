6389c5f08aac39cccf8b8cbf0e8739f77af02533
yuppity <yuppity@wubbalubba.com>
2018-12-23 09:25:45 +0200

    Add support for download recording's thumbnail/snapshot

--- unifi_video/recording.py
+++ unifi_video/recording.py
@@ -6,6 +6,9 @@ from datetime import datetime
 endpoints = {
     'download': lambda x: 'recording/{}/download'.format(x),
     'delete': lambda x: 'recording?recordings[]={}&confirmed=true'.format(x),
+    'snapshot': lambda c, d, r, w: \
+        'snapshot/recording/{}/{}/{}?width={}'.format(c,
+            d.strftime('%Y/%m/%d'), r, w),
 }
 
 class UnifiVideoRecording(UnifiVideoSingle):
@@ -31,13 +34,19 @@ class UnifiVideoRecording(UnifiVideoSingle):
             filename if filename else 'recording-{}-{}.mp4'.format(
                 self._id, self.start_time.isoformat()))
 
+    def snapshot(self, width=600, filename=None):
+        return self._api.get(endpoints['snapshot'](self.cameras[0],
+            self.start_time, self._id, width), filename if filename else \
+                    'recording-{}-{}.jpg'.format(self._id,
+                        self.start_time.isoformat()))
+
     def delete(self):
         return self._api.delete(endpoints['delete'](self._id))
 
     def __str__(self):
         return '{}: {}'.format(type(self).__name__, {
             '_id': self._id,
-            'type': self.rec_type,
+            'rec_type': self.rec_type,
             'start_time': self.start_time.isoformat(),
             'end_time': self.end_time.isoformat()
         })
