fb3c5545734a49968360b231722f34970db660a5
yuppity <yuppity@wubbalubba.com>
2020-09-13 14:10:40 +0300

    Use the RTD Sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
