05924eb925886830262e7c40dc1d8d8b17f6ba30
yuppity <yuppity@wubbalubba.com>
2020-09-16 16:02:31 +0300

    Create util to handle datetimes

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -42,5 +42,70 @@ class UtilitiesTests(unittest.TestCase):
                 test_pair[1],
                 unifi_video.utils.parse_gmt_offset(test_pair[0]))
 
+    def test_dt_resolving(self):
+
+        meaningless_offset = 829869263
+
+        test_pairs = (
+            (('1970-01-01T00:00'      ,          0 * 3600) , 0)          ,
+            (('1970-01-01T01:00'      ,          0 * 3600) , 3.6e3)      ,
+            (('1970-01-01T01:00'      ,          1 * 3600) , 0)          ,
+            (('1970-01-01T01:00'      ,          2 * 3600) , -3.6e3)     ,
+            (((1970, 1, 1, 1, 0)      , 'Europe/Helsinki') , -3.6e3)     ,
+            (('2020-03-03T23:11:22'   ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03T23:11:21'   ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03T23:11'      ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03 23:11:22'   ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03 23:11:21'   ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03 23:11'      ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03T23:11'      ,          0 * 3600) , 1583277060) ,
+            (('20200303T23:11'        ,          0 * 3600) , 1583277060) ,
+            (('20200303T2311'         ,          0 * 3600) , 1583277060) ,
+            (('2020030323:11'         ,          0 * 3600) , 1583277060) ,
+            (('202003032311'          ,          0 * 3600) , 1583277060) ,
+            (('202003032311Z'         ,          0 * 3600) , 1583277060) ,
+            (('202003032311+00:00'    ,          0 * 3600) , 1583277060) ,
+            (('202003032311+03:00'    ,          0 * 3600) , 1583277060) ,
+            (('2020-03-03T23:10:21'   ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03T23:10:21Z'  ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03T23:10:2124' ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03T23:10blah'  ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03T2310blah'   ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03 2310blah'   ,          0 * 3600) , 1583277000) ,
+            (('2020-03-032310blah'    ,          0 * 3600) , 1583277000) ,
+            (('2020-03-03230blah'     ,          0 * 3600) , 1583276400) ,
+            (('2020-aa-032310blah'    ,          0 * 3600) , ValueError) ,
+            (('2020-00-032310blah'    ,          0 * 3600) , ValueError) ,
+            (('a2020-00-032310blah'   ,          0 * 3600) , ValueError) ,
+            (('a020-03-002310blah'    ,          0 * 3600) , ValueError) ,
+            ((0                       ,          0 * 3600) , 0)          ,
+            ((0                       ,          2 * 3600) , 0)          ,
+            (('2019-10-02T23:11:00'   ,          0 * 3600) , 1570057860) ,
+            ((dt(2019, 10, 2, 23, 11) ,          0 * 3600) , 1570057860) ,
+            (((2017, 10, 2, 23, 11)   , 'Europe/Helsinki') , 1506975060) ,
+        )
+
+        for test_pair in test_pairs:
+            try:
+                if isinstance(test_pair[0][1], str):
+                    print(test_pair[0][0])
+                    self.assertEqual(
+                        test_pair[1] * 1000,
+                        unifi_video.utils.dt_resolvable_to_ms(
+                            pytz.timezone(test_pair[0][1]).localize(
+                                dt(*test_pair[0][0])),
+                            meaningless_offset))
+                elif isinstance(test_pair[1], (int, float)):
+                    self.assertEqual(
+                        test_pair[1] * 1000,
+                        unifi_video.utils.dt_resolvable_to_ms(*test_pair[0]))
+                elif issubclass(test_pair[1], Exception):
+                    self.assertRaises(
+                        test_pair[1],
+                        unifi_video.utils.dt_resolvable_to_ms,
+                        *test_pair[0])
+            except AssertionError as e:
+                raise AssertionError('{} ({})'.format(str(e), test_pair[0]))
+
 if __name__ == '__main__':
     unittest.main()
--- unifi_video/utils.py
+++ unifi_video/utils.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from __future__ import print_function, unicode_literals
+from datetime import datetime, timedelta
 import re
 
 def iso_str_to_epoch(iso_str):
@@ -19,6 +20,74 @@ def iso_str_to_epoch(iso_str):
     except Exception:
         raise ValueError('Unable to parse "{}". Use the following ISO ' \
             'format: YYYY-MM-DD HH:MM:SS'.format(iso_str))
+
+def dt_resolvable_to_ms(resolvable, utc_offset=0, resolution=6e4):
+    '''Convert datetime resolvable to milliseconds since the Unix epoch.
+
+    Args:
+        resolvable (datetime or int or str):
+            Object that is resolvable to a date and time (see below)
+
+        utc_offset (int):
+            UTC offset in seconds
+
+        resolution (int):
+            Max resolution (in ms) for the returned timestamp. Default is to
+            mimic UniFi Video frontend in providing at most minute granularity.
+
+    Returns:
+        int: Milliseconds since the Unix epoch
+
+    On *datetime* resolvables
+        The type of the ``resolvable`` parameter will affect the return value.
+
+        *datetime*: Naive *datetime* values are offset by ``utc_offset``
+        while TZ aware *datetime* values are offset by whatever
+        :class:`~datetime.timedelta` their
+        :attr:`datetime.datetime.utcoffset()` returns.
+
+        *str*: Strings are treated as naive *datetime* values and should
+        follow ISO8601 in including at least the ``YYYY``, ``MM``, and ``DD``
+        parts in either ``YYYY-MM-DD`` or ``YYYYMMDD``.
+
+        *int*: Ints are assumed to be UNIX timestamps; only ms conversion
+        and granularity changes will be applied.
+
+    '''
+
+    if isinstance(resolvable, str):
+        iso8601_match = re.match(
+            (
+                r'([0-9]{4})-*([0-9]{2})-*([0-9]{2})[Tt\s]*'
+                r'(?:([0-9]{2}):*){0,1}'
+                r'(?:([0-9]{2}):*){0,1}'
+                r'(?:([0-9]{2}):*){0,1}'
+            ),
+            resolvable)
+        try:
+            return dt_resolvable_to_ms(
+                datetime(*[
+                    int(i) for i in iso8601_match.groups()
+                    if i is not None]),
+                utc_offset,
+                resolution)
+        except (ValueError, AttributeError) as e:
+            raise ValueError(
+                'Unable to parse date and time from "{}"'.format(resolvable))
+    elif isinstance(resolvable, datetime):
+        if resolvable.tzinfo and resolvable.tzinfo.utcoffset(resolvable):
+            utc_offset = resolvable.utcoffset().total_seconds()
+        ts = int(resolvable.strftime('%s'))
+        local_td = datetime.fromtimestamp(ts) - datetime.utcfromtimestamp(ts)
+        ms = (ts + local_td.total_seconds()) * 1000
+    elif isinstance(resolvable, (int, float)):
+        utc_offset = 0
+        ms = resolvable * 1000
+    else:
+        raise TypeError('Datetime resolvable cannot be of type {}'.format(
+            type(resolvable)))
+
+    return int(ms - (ms % resolution) + (-1 * (utc_offset * 1000)))
 
 def format_mac_addr(mac_addr):
     if len(mac_addr) != 12:
